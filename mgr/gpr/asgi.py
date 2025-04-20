8d3b6162b56c342900548855c25427289846583d
Rohit-GP <gp.rohit1201@gmail.com>
2025-04-20 11:42:06 +0530

    Changes inside submodule

---
+++ mgr/gpr/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gpr project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gpr.settings')
+
+application = get_asgi_application()
